7c5457387eff0136692034d8e4beabe815b05f56
Megha <t2lassi12345@gmail.com>
2023-02-22 19:35:44 -0800

    Added support to delete a product

--- dev/backend/controllers/products.controller.js
+++ dev/backend/controllers/products.controller.js
@@ -35,3 +35,61 @@ exports.create = async (req,res) => {
     }
 }
 
+exports.delete = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const productDoc = await ProductModel.findById(id);
+        if(!productDoc){
+            res.status(404).send({
+                message: `Product does not exist in database`
+            });
+        }
+        const brandDoc = await BrandModel.findById(productDoc.brandId);
+        const genderDoc = await GenderModel.findById(productDoc.gender);
+        const categoryDocs = [];
+
+        for(i=0; i<productDoc.categories.length; i++){
+            const category = productDoc.categories[i];
+            const categoryDoc =  await CategoryModel.findById(category);
+            console.log("Category: " + category);
+            categoryDocs.push(categoryDoc);
+        }
+
+        genderDocProdIndex = genderDoc.products.indexOf(id);
+        brandDocProdIndex = brandDoc.products.indexOf(id);
+        for(i=0; i<productDoc.categories.length; i++){
+            const categoryDoc = categoryDocs[i];
+            // console.log("category doc: "+ categoryDoc.products);
+            categoryDocProdIndex = categoryDoc.products.indexOf(id);
+            if(categoryDocProdIndex > -1){
+                categoryDoc.products.splice(categoryDocProdIndex,1);
+            }
+            await categoryDoc.save();
+        }
+        
+        if(genderDocProdIndex > -1){
+            genderDoc.products.splice(genderDocProdIndex,1);
+            await genderDoc.save()
+        }
+        if(brandDocProdIndex > -1){
+            brandDoc.products.splice(brandDocProdIndex,1);
+            await brandDoc.save()
+        }
+
+        const data = await ProductModel.findOneAndDelete({_id : id});
+        
+        if(!data){
+            res.status(404).send({
+                message: ` Cannot delete product with id=${id}. Maybe product was not found`
+            });
+        }else{
+            res.status(200).send({
+                message: `Product was successfully deleted`
+            })
+        }
+
+    }catch(e){
+        console.log(e);
+        res.json({error : e});
+    }
+}
--- dev/backend/routes/products.js
+++ dev/backend/routes/products.js
@@ -6,6 +6,7 @@ const productsController = require("../controllers/products.controller.js");
 //create a new product
 router.route("/").post(productsController.create)
 
-
+//delete a product
+router.route('/:id').delete(productsController.delete);
 
 module.exports=router
