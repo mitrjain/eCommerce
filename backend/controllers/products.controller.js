f169f80aa74d05a008508e65c30ef7fdae1eab85
Megha <t2lassi12345@gmail.com>
2023-02-22 18:51:55 -0800

    Changed the project flow to MVC pattern

---
+++ backend/controllers/products.controller.js
@@ -0,0 +1,37 @@
+
+const ProductModel = require("../models/Product")
+const GenderModel = require("../models/Gender")
+const BrandModel = require("../models/Brand")
+const CategoryModel = require("../models/Category");
+
+exports.create = async (req,res) => {
+    try{
+        const product = new ProductModel(
+            req.body
+        );
+    
+        const savedProduct = await product.save();
+        
+        const genderDoc = await GenderModel.findById(req.body.gender);
+        genderDoc.products.push(product.id);
+        await genderDoc.save();
+    
+        const brandDoc = await BrandModel.findById(req.body.brandId);
+        brandDoc.products.push(product.id);
+        await brandDoc.save();
+    
+        req.body.categories.forEach(async element => {
+            const categoryDoc = await CategoryModel.findById(element);
+            categoryDoc.products.push(product.id);
+            await categoryDoc.save();
+        });
+    
+        res.status(201);
+        res.json(savedProduct);
+    }
+    catch (e){
+        console.log(e);
+        res.json({error:e})
+    }
+}
+
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,41 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-const ProductModel = require("../models/Product")
-const GenderModel = require("../models/Gender")
-const BrandModel = require("../models/Brand")
-const CategoryModel = require("../models/Category");
+const productsController = require("../controllers/products.controller.js");
 
-router.route("/")
-.post( async(req,res)=>{
-    try{
-        const product = new ProductModel(
-            req.body
-        );
+//create a new product
+router.route("/").post(productsController.create)
 
-        const savedProduct = await product.save();
-        
-        const genderDoc = await GenderModel.findById(req.body.gender);
-        genderDoc.products.push(product.id);
-        await genderDoc.save();
 
-        const brandDoc = await BrandModel.findById(req.body.brandId);
-        brandDoc.products.push(product.id);
-        await brandDoc.save();
-
-        req.body.categories.forEach(async element => {
-            const categoryDoc = await CategoryModel.findById(element);
-            categoryDoc.products.push(product.id);
-            await categoryDoc.save();
-        });
-
-        res.status(201);
-        res.json(savedProduct);
-    }
-    catch (e){
-        console.log(e);
-        res.json({error:e})
-    }
-})
 
 module.exports=router
