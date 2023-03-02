9e03d29a005fb4b8eb0ae2d48391d0cf6afa1506
Juhi Malkani <juhimalkani@Juhis-MacBook-Pro.local>
2023-03-02 13:46:10 -0800

    Updated seller page

--- dev/frontend/src/pages/Seller.js
+++ dev/frontend/src/pages/Seller.js
@@ -18,14 +18,34 @@ function Seller() {
                                 <form action="#" className="text-center">
                                     <div>
                                         <label for="productName" className="col-md-2"> Name: </label>
-                                        <input type="text" id="productName" className="col-md-7" placeholder="Enter name of the product"/>
+                                        <input type="text" id="productName" className="col-md-7 py-4" placeholder="Enter name of the product"/>
                                     </div>
                                     <div>
                                         <label for="brandName" className="col-md-2"> Brand Name: </label>
-                                        <select className="dropdown" placeholder="Select brand name: ">
-                                            <option value="Nike"> Nike </option>
-                                            <option value="Puma"> Puma </option>
+                                        <select id="brandName" className="col-md-7 my-4 px-2 py-3" placeholder="Select brand name: ">
+                                            <option selected disabled> Choose a brand </option>
+                                            <option className="dropdown-item" value="Nike"> Nike </option>
+                                            <option className="dropdown-item" value="Puma"> Puma </option>
                                         </select>
+                                    </div>
+                                    <div>
+                                        <label for="desc" className="col-md-2"> Description: </label>
+                                        <input type="text" id="desc" className="col-md-7 py-4" placeholder="Enter description of the product"/>
+                                    </div>
+                                    <div>
+                                        <label for="maxqty" className="col-md-2"> Maximum Quantity: </label>
+                                        <input type="number" id="maxqty" className="my-4 col-md-7 py-2"/>
+                                    </div>
+                                    <div>
+                                        <h4 className="text-left">Specification: </h4>
+                                        <div>
+                                            <label for="productcolor" className="col-md-2"> Color: </label>
+                                            <input type="text" id="productcolor" className="my-4 col-md-7 py-3"/>
+                                        </div>
+                                        <div>
+                                            <label for="productsizes" className="col-md-2"> Sizes: </label>
+                                            <input type="text" id="productcolor" className="col-md-7 py-3"/>
+                                        </div>
                                     </div>
                                 </form>
                             </div>
