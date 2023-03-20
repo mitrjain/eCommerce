29cffa29bce4b4b3965120d2a92f992be5879fde
Juhi Malkani <juhimalkani@Juhis-MacBook-Pro.local>
2023-03-19 21:39:10 -0700

    Resolve merge conflicts

--- dev/frontend/src/index.js
+++ dev/frontend/src/index.js
@@ -5,13 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-<<<<<<< Updated upstream
 root.render(<App />);
-=======
-root.render(
-  <App />
-);
->>>>>>> Stashed changes
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
