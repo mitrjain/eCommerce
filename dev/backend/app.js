84e8c0bce36dcabc17dc00a0a6c7b20ebba4e618
Juhi Malkani <juhimalkani@Juhis-MacBook-Pro.local>
2023-03-19 21:36:59 -0700

    Integrate some APIs in seller page

--- dev/backend/app.js
+++ dev/backend/app.js
@@ -1,14 +1,14 @@
-const express =  require('express')
+const express = require('express')
 
 const app = express()
 const mongoose = require('mongoose');
 require('dotenv/config')
 mongoose.set('strictQuery', false);
 mongoose.connect(`${process.env.DB_PROTOCOL}//${process.env.DB_USERNAME}:${process.env.DB_PASSSOWRD}@${process.env.DB_HOST}/${process.env.DB_NAME}?${process.env.DB_OPTIONS}`)
-.then(()=>{
-    console.log("Connected to MongoDB Cloud")
-})
-.catch(error => console.log(error));
+    .then(() => {
+        console.log("Connected to MongoDB Cloud")
+    })
+    .catch(error => console.log(error));
 
 app.use(express.json())
 
@@ -18,12 +18,12 @@ const genderRouter = require("./routes/genders")
 const categoryTypesRouter = require("./routes/categoryTypes")
 
 
- app.use("/products",productsRouter)
+app.use("/products", productsRouter)
 app.use("/brands", brandsRouter)
 app.use("/genders", genderRouter)
 app.use("/categoryTypes", categoryTypesRouter)
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("Home of Gladiators API");
 });
 
--- dev/frontend/src/pages/Seller.js
+++ dev/frontend/src/pages/Seller.js
@@ -1,104 +1,163 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
+import Select from 'react-select';
+import axios from "axios";
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 
-
-function Seller() {
-    const sizeArray = ['5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12'];
-
-    const [checked, setChecked] = useState([]);
-    const [categoryType, setCategoryType] = useState([]);
-    const [specsDiv, setSpecsDiv] = useState([]);
-
-    // Add/Remove checked item from list
-
-    const handleChangeCategoryType = (event) => {
-        setCategoryType(event.target.value);
-    };
-
-    const handleCheck = (event) => {
-        var updatedList = [...checked];
-        if (event.target.checked) {
-            updatedList = [...checked, event.target.value];
-        } else {
-            updatedList.splice(checked.indexOf(event.target.value), 1);
+export default class Seller extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            selectedCategoryType: "",
+            brands: [],
+            categoryTypes: [],
+            specsDiv: [],
+            checked: [],
+            sizeArray: ['5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12'],
+            categories: []
         }
-        setChecked(updatedList);
-    };
+    }
 
-    const addDivToSpecsDiv = () => {
-        if (specsDiv.length == 0) {
-            setSpecsDiv([{
-                spec:
-                    <div>
-                        <div>
-                            <label for="productcolor" className="col-md-2"> Color: </label>
-                            <input type="text" id="productcolor" className="my-4 col-md-7 py-3" />
-                        </div>
-                        <div>
-                            <label for="sizes" className="col-md-2"> Sizes: </label>
-                            {
-                                sizeArray.map((item, index) => (
-                                    <div key={index}>
-                                        <input value={item} type="checkbox" onChange={handleCheck}></input>
-                                        <span> {item} </span>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        <div>
-                            <label htmlFor="price" className="col-md-2"> Price: </label>
-                            <input type="number" id="price" className="my-4 col-md-7 py-2" />
-                        </div>
-                    </div>
-            }])
-        } else {
-            setSpecsDiv((prev) => [...prev, {
-                spec:
-                    <div>
-                        <div>
-                            <label for="productcolor" className="col-md-2"> Color: </label>
-                            <input type="text" id="productcolor" className="my-4 col-md-7 py-3" />
-                        </div>
-                        <div>
-                            <label for="sizes" className="col-md-2"> Sizes: </label>
-                            {
-                                sizeArray.map((item, index) => (
-                                    <div key={index}>
-                                        <input value={item} type="checkbox" onChange={handleCheck}></input>
-                                        <span> {item} </span>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        <div>
-                            <label htmlFor="price" className="col-md-2"> Price: </label>
-                            <input type="number" id="price" className="my-4 col-md-7 py-2" />
-                        </div>
-                    </div>
-            }])
-        }
-
-    };
-
-    const removeDivFromSpecsDiv = () => {
-        setSpecsDiv(specsDiv.filter(item => specsDiv.indexOf(item) !== specsDiv.length - 1));
-    };
-
-    // Generate string of checked items
-    var checkedItems = checked.length
-        ? checked.reduce((total, item) => {
-            return total + ", " + item;
+    setSpecsDiv(specsDivParam) {
+        this.setState({
+            specsDiv: specsDivParam
         })
-        : "";
+    }
 
-    var isChecked = (item) =>
-        checked.includes(item) ? "checked-item" : "not-checked-item";
+    setChecked(checkedParam) {
+        this.setState({
+            checked: checkedParam
+        })
+    }
 
-    return (
-        <Fragment>
-            <div id="seller">
+    setBrands(brandsParam) {
+        this.setState({
+            brands: brandsParam
+        })
+    }
+
+    setCategoryTypes(categoryTypesParam) {
+        this.setState({
+            categoryTypes: categoryTypesParam
+        })
+    }
+
+    setSelectedCategoryType = (input) => {
+        this.setState({
+            selectedCategoryType: input.label,
+            categories: []
+        })
+        let categoriesArray = [];
+        for (let i = 0; i < input.categories.length; i++) {
+            categoriesArray.push({ label: input.categories[i].name, value: input.categories[i].name })
+        }
+        this.setState({
+            categories: categoriesArray
+        })
+        console.log(input);
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/brands')
+            .then((response) => {
+                let brands = [];
+                for (let i = 0; i < response.data.length; i++) {
+                    var brand = { label: response.data[i].name, value: response.data[i].name }
+                    brands.push(brand);
+                }
+                this.setBrands(brands);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+        axios.get('http://localhost:3001/categoryTypes')
+            .then((res) => {
+                let categoryTypes = [];
+                for (var i = 0; i < res.data.length; i++) {
+                    var categoryType = { label: res.data[i].name, value: res.data[i].name, categories: res.data[i].categories }
+                    categoryTypes.push(categoryType);
+                }
+                this.setCategoryTypes(categoryTypes);
+                this.setSelectedCategoryType(categoryTypes[0]);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    handleCheck(event) {
+        var updatedList = [...this.checked];
+        if (event.target.checked) {
+            updatedList = [...this.checked, event.target.value];
+        } else {
+            updatedList.splice(this.checked.indexOf(event.target.value), 1);
+        }
+        this.setChecked(updatedList);
+    }
+
+    addDivToSpecsDiv = () => {
+        if (this.state.specsDiv.length == 0) {
+            this.setSpecsDiv([{
+                spec:
+                    <div>
+                        <span className="form-inline">
+                            <label htmlFor="productcolor" className="col-md-2"> Color: </label>
+                            <input type="text" id="productcolor" className="col-md-7 px-3 py-2 my-4" />
+                        </span>
+                        <span className="form-inline form-check-inline">
+                            <label htmlFor="sizes" className="col-md-2"> Sizes: </label>
+                            {
+                                this.state.sizeArray.map((item, index) => (
+                                    <div key={index} className="px-2">
+                                        <input value={item} type="checkbox" onChange={this.handleCheck}></input>
+                                        < span > {item} </span>
+                                    </div>
+                                ))
+                            }
+                        </span >
+                        <span className="form-inline">
+                            <label htmlFor="price" className="col-md-2"> Price: </label>
+                            <input type="number" id="price" className="col-md-7 px-3 py-2 my-4" />
+                        </span>
+                    </div >
+            }])
+        } else {
+            this.setSpecsDiv(this.state.specsDiv.concat([{
+                spec:
+                    <div>
+                        <span className="form-inline">
+                            <label htmlFor="productcolor" className="col-md-2"> Color: </label>
+                            <input type="text" id="productcolor" className="col-md-7 px-3 py-2 my-4" />
+                        </span>
+                        <span className="form-inline form-check-inline">
+                            <label htmlFor="sizes" className="col-md-2 "> Sizes: </label>
+                            {
+                                this.state.sizeArray.map((item, index) => (
+                                    <div key={index} className="px-2">
+                                        <input value={item} type="checkbox" onChange={this.handleCheck}></input>
+                                        <span> {item} </span>
+                                    </div>
+                                ))
+                            }
+                        </span>
+                        <span className="form-inline">
+                            <label htmlFor="price" className="col-md-2"> Price: </label>
+                            <input type="number" id="price" className="col-md-7 px-3 py-2 my-4" />
+                        </span>
+                    </div>
+            }]))
+        }
+    }
+
+    removeDivFromSpecsDiv() {
+        this.setSpecsDiv(this.specsDiv.filter(item => this.specsDiv.indexOf(item) !== this.specsDiv.length - 1));
+    }
+
+    render() {
+        return (
+            <div>
                 <nav className="colorlib-nav" role="navigation">
                     <div className="top-menu">
                         <div className="container">
@@ -107,70 +166,70 @@ function Seller() {
                             <h1 className="text-center m-5"> Want to sell another product? Add one here </h1>
                             <div>
                                 <form action="#">
-                                    <div>
+                                    <span className="form-inline">
                                         <label htmlFor="productName" className="col-md-2"> Name: </label>
-                                        <input type="text" id="productName" className="col-md-7 py-4" placeholder="Enter name of the product" />
-                                    </div>
-                                    <div>
+                                        <input type="text" id="productName" className="col-md-7 px-3 py-2 my-4" placeholder="Enter name of the product" />
+                                    </span>
+                                    <span className="form-inline">
                                         <label htmlFor="brandName" className="col-md-2"> Brand Name: </label>
-                                        <select id="brandName" className="col-md-7 my-4 px-2 py-3" placeholder="Select brand name: ">
-                                            <option selected disabled> Choose a brand </option>
-                                            <option className="dropdown-item" value="Nike"> Nike </option>
-                                            <option className="dropdown-item" value="Puma"> Puma </option>
-                                        </select>
-                                    </div>
-                                    <div>
+                                        <Select id="brandName" className="col-md-7 px-3 py-2" options={this.state.brands} value={this.state?.brands[0]} placeholder="Select brand name: " />
+                                    </span>
+                                    <span className="form-inline">
                                         <label htmlFor="desc" className="col-md-2"> Description: </label>
-                                        <input type="text" id="desc" className="col-md-7 py-4" placeholder="Enter description of the product" />
-                                    </div>
-                                    <div>
+                                        <input type="text" id="desc" className="col-md-7 px-3 py-2 my-4" placeholder="Enter description of the product" />
+                                    </span>
+                                    <span className="form-inline">
                                         <label htmlFor="maxqty" className="col-md-2"> Maximum Quantity: </label>
-                                        <input type="number" id="maxqty" className="my-4 col-md-7 py-2" />
-                                    </div>
+                                        <input type="number" id="maxqty" className="col-md-7 px-3 py-2 my-4" />
+                                    </span>
                                     <div>
                                         <div className="row">
                                             <h4 className="text-left">Specification: </h4>
                                             <div>
-                                                <button className="mx-2" onClick={addDivToSpecsDiv}> Add </button>
-                                                <button onClick={removeDivFromSpecsDiv}> Remove </button>
+                                                <button className="mx-2" onClick={this.addDivToSpecsDiv}> Add </button>
+                                                <button onClick={this.removeDivFromSpecsDiv}> Remove </button>
                                             </div>
                                         </div>
                                         <div>
                                             {
-                                                specsDiv.length == 0 ? addDivToSpecsDiv() : ""
+                                                this.state.specsDiv.length == 0 ? this.addDivToSpecsDiv() : ""
                                             }
                                             {
-                                                specsDiv.map((item, index) => (
-                                                    item.spec
-                                                ))
+                                                this.state.specsDiv.map((item, index) => {
+                                                    return (
+                                                        <div>
+                                                            {item.spec}
+                                                        </div>
+                                                    )
+                                                })
                                             }
                                         </div>
                                     </div>
-                                    <div>
-                                        <div>
-                                            <label htmlFor="categoryTypeId" className="col-md-2"> Category Type: </label>
-                                            <select id="categoryTypeId" className="col-md-7 my-4 px-2 py-3" placeholder="Select Category Type: " onChange={handleChangeCategoryType} >
-                                                <option selected disabled> Choose a category </option>
-                                                <option className="dropdown-item" value="Style"> Style </option>
-                                                <option className="dropdown-item" value="Occasion"> Occasion </option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <label htmlFor="category" className="col-md-2">  {categoryType} </label>
-                                        <select id="category" className="col-md-7 my-4 px-2 py-3" placeholder="Select Category: ">
-                                            <option selected disabled> Choose  </option>
-                                            <option className="dropdown-item" value="Slip-on"> Slip-on </option>
-                                            <option className="dropdown-item" value="Sandal"> Sandal </option>
-                                        </select>
-                                    </div>
+                                    <span className="form-inline">
+                                        <label htmlFor="categoryTypeId" className="col-md-2"> Category Type: </label>
+                                        <Select id="categoryTypeId" options={this.state.categoryTypes} className="col-md-7 px-3 py-2" value={this.state?.categoryTypes[0]} placeholder={this.state.categoryTypes.placeholder} onChange={this.setSelectedCategoryType} />
+                                    </span>
+                                    <span className="form-inline">
+                                        <label htmlFor="category" className="col-md-2"> {this.state.selectedCategoryType} </label>
+                                        <Select id="category" value={this.state?.categories[0]} options={this.state.categories} className="col-md-7 px-3 py-2 my-4" />
+                                    </span>
                                 </form>
                             </div>
                         </div>
                     </div >
                 </nav >
-            </div >
-        </Fragment >
-    );
+            </div>
+        );
+    }
 }
-export default Seller;
+
+//     // Generate string of checked items
+//     var checkedItems = checked.length
+//         ? checked.reduce((total, item) => {
+//             return total + ", " + item;
+//         })
+//         : "";
+
+//     var isChecked = (item) =>
+//         checked.includes(item) ? "checked-item" : "not-checked-item";
+
