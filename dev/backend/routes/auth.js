7a0996665c9e908af7bcd404d231b8591780972d
Mit <mitrjain@gmail.com>
2023-04-07 18:34:51 -0700

    In Progress APIs: POST /auth/loginWithCreds, POST /auth/loginWithToken, POST /auth/signup

--- dev/backend/routes/auth.js
+++ dev/backend/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const authController = require("../controllers/auth.controller.js")
 
 router.route("/login")
-.post(authController.login)
+.post(authController.loginWithCredentials)
 
 router.route("/signup")
 .post(authController.signup)
